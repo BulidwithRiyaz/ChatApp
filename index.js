d1caa0e38f9c3affe30eacfa1ea28c2ab46ddca6
BulidwithRiyaz <dev.riyaz.code@gmail.com>
2025-08-25 22:44:29 +0530

    error custom  handling added  using middlewares

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path =require("path");
 const mongoose = require('mongoose');
 const methodOverride =require("method-override");
 const Chat =require("./model/chat.js");
+const ExpressError =require("./ExpressError.js");
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
@@ -20,12 +21,21 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/chatapp');
 }
 
-app.use((req,res,next) =>{
-    req.responseTime = new Date().toString();
-    console.log(req.method,req.path,req.hostname);
-    console.log(req.responseTime);
-    return next();
-})
+// app.use((req,res,next) =>{
+//     req.responseTime = new Date().toString();
+//     console.log(req.method,req.path,req.hostname);
+//     console.log(req.responseTime);
+//     return next();
+// })
+
+
+
+// wrapAsync error function
+function wrapAsync(fn){
+    return function(req,res,next){
+        fn(req,res,next).catch((err) => next(err));
+    }
+}
 
 const accessToken = (req,res,next) =>{
     let {token} =req.query;
@@ -41,10 +51,20 @@ app.get("/",accessToken,(req,res)=>{
 
 // index route
 
-app.get("/chats",async (req,res) => {
+app.get("/chats",wrapAsync(async (req,res) => {
     let chats = await Chat.find();
     res.render("index.ejs",{chats});
-})
+}));
+
+// new- async err route
+app.get("/chats/:id",wrapAsync(async(req,res,next)=>{
+    let {id}=req.params;
+    let chat = await Chat.findById(id);
+    if(!chat){
+        return next(new ExpressError(404,'Chat not found'));
+    }
+    res.render("edit.ejs",{chat})
+}));
 
 // new route 
 app.get("/chats/new",(req,res)=>{
@@ -68,30 +88,35 @@ app.post("/chats",(req,res)=>{
 })
 
 // edit route
-app.get("/chats/:id/edit", async(req,res)=>{
+app.get("/chats/:id/edit", wrapAsync( async(req,res)=>{
     let {id} =req.params;
     let chat = await Chat.findById(id);
     res.render("edit.ejs",{chat});
     console.log(chat);
-})
+}));
 
 //update route
-app.put("/chats/:id", async(req,res)=>{
+app.put("/chats/:id", wrapAsync(async(req,res)=>{
     let {id} =req.params;
     let {msg : msg} =req.body;
     let updateChat = await Chat.findByIdAndUpdate(id,{msg : msg} ,{new :true}, {runValidator: true});
     res.redirect("/chats")
-})
+}));
 
 // delete route 
-app.delete("/chats/:id", async(req,res)=>{
+app.delete("/chats/:id", wrapAsync(async(req,res)=>{
     let {id} =req.params;
     let deleteChat = await Chat.findByIdAndDelete(id);
     console.log(deleteChat);
     res.redirect('/chats')
+}));
+
+
+// error handler
+app.use((err,req,res,next)=>{
+    let {status=500,message="Some Error"}=err;
+    res.status(status).send(message);
 })
-
-
 
 app.listen(8080,()=>{
     console.log("app is listing at post 8080");
